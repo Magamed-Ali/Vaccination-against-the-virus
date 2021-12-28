53c125b47c994a9e15077e3a909075fe544403da
gazmagamaev <magamedali93@mail.ru>
2021-12-28 13:53:28 +0300

    carousel

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import pharmacy from './images/Pngtre.png'
 import nav from './images/premium-icon-NAV.png'
-import x from './images/premium-icon-X.png'
 import s from './navbar.module.scss';
 import {NavLink} from "react-router-dom";
 
@@ -25,37 +24,46 @@ function Navbar(props) {
                 <NavLink to="/historyOfVaccinated">HistoryOfVac-ed</NavLink>
             </div>
         </div>*/}
-            <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><img className={s.logo} src={pharmacy}/></a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">
-                                    <NavLink to="/home" className={s.navLink}>Home</NavLink></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">
-                                    <NavLink to="/form" className={s.navLink}>Form</NavLink></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">
-                                    <NavLink to="/vaccinated" className={s.navLink}>Vaccinated</NavLink></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="#" tabIndex="-1"
-                                   aria-disabled="true">
-                                    <NavLink to="/historyOfVaccinated" className={s.navLink}>HistoryOfVac-ed</NavLink></a>
-                            </li>
-                        </ul>
+
+            <div className="container">
+                <div className="row">
+
+                    <div className={s.bb}>
+                        <nav className="navbar navbar-expand-sm">
+                            <a className="navbar-brand" href="#"> <img className={s.logo}
+                                                                       src={pharmacy}/></a>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#navbarNav" aria-controls="navbarNav"
+                                    aria-expanded="false"
+                                    aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarNav">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                        <a className="nav-link active" aria-current="page" href="#">
+                                            <NavLink to="/home" className={s.navLink}> Home </NavLink></a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">
+                                            <NavLink to="/form" className={s.navLink}> Form </NavLink></a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">
+                                            <NavLink to="/vaccinated"
+                                                     className={s.navLink}> Vaccinated </NavLink></a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">
+                                            <NavLink to="/historyOfVaccinated"
+                                                     className={s.navLink}> History</NavLink></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
                     </div>
                 </div>
-            </nav>
+            </div>
 
 
         </>
