93791d420e685823ee459929f41ea0caff0d4158
gazmagamaev <magamedali93@mail.ru>
2022-01-02 19:12:33 +0300

    modify header nav

--- src/components/shared/header/HeaderNav.js
+++ src/components/shared/header/HeaderNav.js
@@ -1,59 +1,33 @@
 import React from 'react';
 import logo from '../../../assets/images/logo-white.png';
-import styles from '../../../styles/style.scss';
+
 
 function HeaderNav() {
     return (
-        <div className="headerNav">
-            <div className="container-fluid p-0 text-center ">
-                    <div className="row align-items-center">
-                        <div className="col-3"><img src={logo} alt=""/></div>
-                        <div className="col">
-                            <div className="container-fluid">
-                                <div className="row">
-                                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                                        <div className="container-fluid">
-                                            <button className="navbar-toggler " type="button" data-bs-toggle="collapse"
-                                                    data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                                                    aria-label="Toggle navigation">
-                                                <span className="navbar-toggler-icon"></span>
-                                            </button>
-                                            <div className="collapse navbar-collapse" id="navbarNav">
-                                               <div className="container-fluid">
-                                                   <ul className="navbar-nav">
-                                                       <li className="nav-item">
-                                                           Home
-                                                       </li>
-                                                       <li className="nav-item">
-                                                           Features
-                                                       </li>
-                                                       <li className="nav-item">
-                                                           Pricing
-                                                       </li>
-                                                       <li className="nav-item">
-                                                           Disabled
-                                                       </li>
-                                                   </ul>
-
-                                               </div>
-                                            </div>
-                                        </div>
-                                    </nav>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-3">
-                            <button>
-                                asdfas
-                            </button>
-                        </div>
+        <div className="container-fluid nav-bar-wrapper">
+            <div className="container">
+                <div className="nav-bar">
+                    <div className="nav-bar__logo">
+                        <img src={logo} alt="logo"/>
                     </div>
+                    <div className="nav-bar__menu">
+                        <ul>
+                            <li>Home</li>
+                            <li>Form</li>
+                            <li>Vaccinated</li>
+                            <li>History</li>
+                        </ul>
+                    </div>
+                    <div className="nav-bar__btn">
+                        <button className="nav-bar__btn__button btn">Find A Doctor</button>
+                    </div>
+                </div>
             </div>
 
         </div>
 
 
-);
+    );
 }
 
 export default HeaderNav;
