13bcdae9ae201afbbc3c53998b7d3fc491375ef1
gazmagamaev <magamedali93@mail.ru>
2022-01-25 12:01:00 +0300

    remove class isShow-btn

--- src/components/shared/scrollToTop/ScrollToTop.js
+++ src/components/shared/scrollToTop/ScrollToTop.js
@@ -4,27 +4,23 @@ import {useState} from "react";
 
 function ScrollToTop(props) {
 
-    const eventListener = () => {
-        window.scrollTo(0, 0)
+    const eventListener = () => window.scrollTo(0, 0);
+
+
+    let [isShowBtn, setIsShowBtn] = useState();
+
+    window.onscroll = () => {
+        window.scrollY > 500
+            ? setIsShowBtn('scroll-top')
+            :
+            setIsShowBtn('')
     }
 
-    let[isShowBtn, setIsShowBtn] = useState('isShow-btn__hide');
-
-      window.onload = () => {
-
-        window.onscroll = () => {
-            if(window.scrollY > 500){
-                setIsShowBtn('isShow-btn')
-            }else {
-                setIsShowBtn('isShow-btn__hide')
-            }
-        }
-    }
 
     return (
-        <div >
-            <div  id="scroll-top" className={isShowBtn} onClick={() => eventListener()}>
-                <FontAwesomeIcon icon={faArrowUp} />
+        <div>
+            <div id={isShowBtn}  onClick={() => eventListener()}>
+                <FontAwesomeIcon icon={faArrowUp}/>
             </div>
         </div>
     );
