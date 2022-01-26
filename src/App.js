fd988b13ca4505bf39582ac6d7312489aa66dadf
gazmagamaev <magamedali93@mail.ru>
2022-01-26 22:13:40 +0300

    Saerch Modal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Home from "./components/pages/home/Home";
 import {Route, Routes} from "react-router-dom";
 import ContactUs from "./components/pages/contactUs/ContactUs";
-import ScrollToTop from "./components/shared/scrollToTop/ScrollToTop";
 
 function App() {
+
+    let [modal, setModal] = useState(false);
+
+    let handleClick = () => setModal(!modal);
 
     return (
         <>
             <Routes>
-                <Route path='/' exact element={<Home/>}/>
-                <Route path='/contact' element={<ContactUs/>}/>
+                <Route path='/' exact element={<Home isOpened={modal} handleClick={handleClick}/>}/>
+                <Route path='/contact' element={<ContactUs isOpened={modal} handleClick={handleClick}/>}/>
             </Routes>
-            <ScrollToTop/>
         </>
     );
 }
--- src/components/pages/contactUs/ContactUs.js
+++ src/components/pages/contactUs/ContactUs.js
@@ -5,18 +5,20 @@ import Footer from "../../shared/footer/Footer";
 import PageTitleContact from "../../shared/pageTitleContact/PageTitleContact";
 import ContactUsLine from "../../shared/container-us-lines/ContainerUsLine"
 import MakeRequest from "../../shared/form/MakeRequest";
+import SearchModal from "../../shared/search-modal/SearchModal";
 
-function ContactUs() {
+function ContactUs(props) {
     return (
         <div>
             <div className="container">
                 <Topbar/>
-                <HeaderNav/>
+                <HeaderNav handleClick={props.handleClick}/>
             </div>
             <PageTitleContact/>
             <ContactUsLine/>
             <MakeRequest/>
             <Footer/>
+            <SearchModal isOpened={props.isOpened} handleClick={props.handleClick}/>
         </div>
     );
 }
